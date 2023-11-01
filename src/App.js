0fafbe8813ff242b71c0573c5c16f40537828b9c
Shinya Aoi <saoi@Shinyas-MacBook-Pro.local>
2023-11-01 14:18:41 -0700

    package card component

--- src/App.js
+++ src/App.js
@@ -3,11 +3,17 @@ import './App.css';
 import Header from './components/Header';
 import "bootstrap/dist/css/bootstrap.min.css";
 import "bootstrap/dist/js/bootstrap.bundle.min";
+import PackageCard from './components/PackageCard';
 
 function App() {
   return (
     <div className="App">
       <Header />
+      <PackageCard
+        title="Granville Island Beer Hopping"
+        location="Granville Island, Vancouver"
+        desc="Nestled in the heart of Vancouver, Granville Island is a thriving cultural enclave. This unique destination boasts a dynamic blend of art, culinary delights, and outdoor beauty. Explore the vibrant arts scene in galleries and theaters. Savor local seafood and gourmet treats at the bustling Public Market. Discover one-of-a-kind treasures in artisan boutiques. Enjoy scenic waterfront views, embark on outdoor adventures, and immerse yourself in the island's rich history. Family-friendly and hosting events year-round, Granville Island is a captivating, multi-faceted destination where urban charm meets creative spirit, offering an unforgettable taste of Vancouver's diversity and vitality."
+        id="1" />
     </div>
   );
 }
---
+++ src/components/PackageCard.js
@@ -0,0 +1,27 @@
+import React from "react"
+import '../css/PackageCard.css'
+
+/**
+ * PackageCard components should have
+ * - Name
+ * - Location
+ * - Description
+ * - id (no display)
+ */
+const PackageCard = (props) => {
+  return (
+    <div className="card">
+      <img className="card-img-top" src="https://fakeimg.pl/440x320/282828/eae0d0/?retina=1" alt="tour image" />
+      <div className="card-body">
+        <h5 className="card-title">{props.title}</h5>
+        <div className="">{props.location}</div>
+        <div className="card-text">{props.desc}</div>
+      </div>
+      <div className="card-footer">
+        <a className="btn btn-primary" href={`/tours/:${props.id}`}>more</a>
+      </div>
+    </div>
+  )
+}
+
+export default PackageCard;
