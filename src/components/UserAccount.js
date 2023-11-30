5afa6469cd641b71d727873ac9ac7f7e0eef391f
Shinya Aoi <saoi@Shinyas-MacBook-Pro.local>
2023-11-29 23:20:21 -0800

    add booking fetch

--- src/components/UserAccount.js
+++ src/components/UserAccount.js
@@ -5,31 +5,62 @@ import config from '../config';
 import axios from 'axios';
 
 const UserAccount = () => {
+  const tabs = ["Profile", "Booking", "Setting"];
   const [travelBuddy, setTravelBuddy] = useState();
+  const [bookings, setBookings] = useState([]);
+  const [activeTab, setActiveTab] = useState(0);
   const { travelBuddyId } = useParams();
   useEffect(() => {
     axios.get(config.apiUrl + '/api/travelBuddy/' + travelBuddyId)
       .then(response => {
         setTravelBuddy(response.data);
-        console.log(travelBuddy);
+      })
+      .catch(error => {
+        console.log(error)
+      })
+
+      axios.get(config.apiUrl + '/api/booking', {
+        params: {
+          travelBuddyID: travelBuddyId
+        }
+      })
+      .then(response => {
+        setBookings(response.data);
       })
       .catch(error => {
         console.log(error)
       })
   }, []);
 
+  const changeTab = (id) => {
+    setActiveTab(id);
+  }
+
+  const activeNav = (id) => {
+    let el = "menu-item";
+    if (id === activeTab) {
+      el += " active"
+    }
+    return el;
+  }
+
   return (
     <div className="account-container">
       <aside className="sidebar">
         <nav className="navigation">
           <ul className="menu">
-            <li className="menu-item active"><a href="#">Profile</a></li>
-            <li className="menu-item"><a href="#">Bookings</a></li>
-            <li className="menu-item"><a href="#">Settings</a></li>
+            {tabs.map((tab, id) => (
+              <li className={activeNav(id)}
+                key={tab} >
+                  <a href="#"
+                    onClick={() => changeTab(id)}>{tab}
+                  </a>
+              </li>
+            ))}
           </ul>
         </nav>
       </aside>
-      {travelBuddy === undefined ? <p>Loading</p> :
+      {activeTab === 0 && travelBuddy !== undefined ?
         <main className="profile-content">
           <section className="profile-section">
             <h2>Personal Information</h2>
@@ -50,7 +81,36 @@ const UserAccount = () => {
               </form>
           </section>
         </main>
+        : null
       }
+      {activeTab === 1 && bookings !== undefined ?
+        <main className="profile-content">
+          <h2>Booking Information</h2>
+          <table id="travelBuddyTable" className="table table-hover table-bordered">
+            <thead className="thead-dark">
+              <tr>
+                <th scope="col">Booking ID</th>
+                <th scope="col">Tour Name</th>
+                <th scope="col">Booking Date</th>
+                <th scope="col">Start Time</th>
+              </tr>
+            </thead>
+            <tbody>
+              {bookings.map((booking) => {
+                return (
+                  <tr>
+                    <td>{booking.bookingID}</td>
+                    <td>{booking.bookingID}</td>
+                    <td>{booking.bookingDate}</td>
+                    <td>{booking.startTime}</td>
+                  </tr>
+                );
+              })}
+            </tbody>
+          </table>
+        </main>
+      : null}
+      
     </div>
   );
 };
